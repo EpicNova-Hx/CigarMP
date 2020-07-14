7662a02e8fab2a1395ebcb142c41151d1d2ba995
EpicNova-Hx <66078147+EpicNova-Hx@users.noreply.github.com>
2020-07-14 19:39:53 +0700

    Add files via upload

---
+++ p.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+clear
+figlet -tc "CigarMP" | lolcat
+echo $i "========================================"
+echo " Author : Manca "
+echo " Youtube: EpicNova "
+echo " Github : EpicNova-Hx "
+echo " ========================================"
+echo
+echo "——————————————————————"
+echo "|" $bi "["$i"1"$bi"]"$i"Install Future"
+echo $i"——————————————————————"
+echo $ku
+echo -n "CMP" $me"·> "$pur
+read cmp
+#1
+if [ $cmp = 1 ]
+then
+Future.sh
+fi
